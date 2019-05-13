0b6aea967a004d8ed09beb8a4533b5350810e2ea
brianla29 <brla@ucsc.edu>
2019-05-13 03:31:28 -0700

    background is responsive and image is set

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import waterdistrict from './waterdistrict.jpg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Brian La
-        </p>
-        <a
-          className="App-link"
-          href="https://www.linkedin.com/in/brian-la-392150133/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Connect with me!
+class App extends React.Component {
+
+  render = () => {
+
+    return (
+      <div className="App">
+        <header className="App-header" style={{
+          backgroundImage: 'url(' + waterdistrict + ')',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          overflow: 'hidden',
+        }}>
+          <h1 className='black'>
+            Brian La
+        </h1>
+          <a
+            className="App-link"
+            href="https://www.linkedin.com/in/brian-la-392150133/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Connect with me!
         </a>
-      </header>
-    </div>
-  );
+        </header>
+        <div className="tabs">
+          <h1>
+            About me
+        </h1>
+          <h1>
+            What I bring to the table
+        </h1>
+          <h1>
+            Contact
+        </h1>
+        </div>
+      </div>
+    );
+  }
 }
+
 
 export default App;

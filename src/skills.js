881743d258406a801b14914310aa3bab4bcaa56e
brianla29 <brla@ucsc.edu>
2019-09-17 19:05:04 -0700

    skills done but exp needs to be finished

--- src/skills.js
+++ src/skills.js
@@ -12,10 +12,43 @@ class Skills extends React.Component {
                 </div>
                 <div className="containerColumn">
                     <div className="twoColumn">
-                        <h1 id="underline">Skills</h1>
+                        <h1 id="title">Skills</h1>
+                        <h2 id="subTitles">Programming Languages</h2>
+                        <ul>
+                            <li id="left">
+                                Java, C, Javascript, HTML, CSS, Powershell, MIPS Assembly
+                            </li>
+                        </ul>
+                        <h2 id="subTitles">Technologies</h2>
+                        <ul>
+                            <li id="left">
+                                React Native, Redux, React, Firebase, FireStore, Android Studio, VirtualBox, Npm
+                            </li>
+                        </ul>
+                        <h2 id="subTitles">Classes Taken</h2>
+                        <ul>
+                            <li id="left">
+                                Algorithms and Abstract Data Types, Introduction to Analysis of Algorithms, Computational Models(Theory of Computation),
+                                Comparative Programming Languages, Computer Systems & Assembly Language, Data Structures, Mobile Applications(Android), Discrete Mathematics,
+                                Computer Archictecture, Operating Systems, Introduction to Software Engineering, Probability Theory
+                            </li>
+                        </ul>
                     </div>
                     <div className="twoColumn">
-                        <h1 id="underline">Experience</h1>
+                        <h1 id="title">Experience</h1>
+                        <h2 id="subTitles">Professional</h2>
+                        <ul>
+                            <li>
+                                <h3>Santa Clara Valley Water District</h3>
+                                <h4>Information Technology Intern(Networks & Infrastructure)</h4>
+                            </li>
+                        </ul>
+                        <h2 id="subTitles">Projects</h2>
+                        <ul>
+                            <li>
+
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>

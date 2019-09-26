980c0a87922bf90af09d374f72f985ab8cf3b3d1
brianla29 <brla@ucsc.edu>
2019-09-26 14:27:59 -0700

    Updates

---
+++ precache-manifest.a6ed71163cf5e0580a6a90105ad374d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc5a9b37f146339baa90658ae160e5f9",
+    "url": "/brianla29.github.io/index.html"
+  },
+  {
+    "revision": "ea37b5a22e0920f2b880",
+    "url": "/brianla29.github.io/static/css/main.f94dd4b3.chunk.css"
+  },
+  {
+    "revision": "017e8464425f9de8f1d3",
+    "url": "/brianla29.github.io/static/js/2.37c3dba2.chunk.js"
+  },
+  {
+    "revision": "ea37b5a22e0920f2b880",
+    "url": "/brianla29.github.io/static/js/main.bbc6c547.chunk.js"
+  },
+  {
+    "revision": "e7b4995f2d6eff5128c4",
+    "url": "/brianla29.github.io/static/js/runtime~main.1b805c98.js"
+  },
+  {
+    "revision": "467a51c9c7d475a55db132bc57898d51",
+    "url": "/brianla29.github.io/static/media/homeBackground.467a51c9.jpg"
+  }
+]);
